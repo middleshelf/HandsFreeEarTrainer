ca832dff64e68ba614a84082ef3b8664f453aa06
Walt Austin <walta@slalom.com>
2015-07-05 15:48:07 -0400

    Cleaned up unused code and made more descriptive comments Removed PlayMidi file Removed PlayMidi File References

--- app/src/main/java/com/middleshelf/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/middleshelf/myfirstapp/DisplayMessageActivity.java
@@ -29,21 +29,28 @@ import java.util.Random;
 
 public class DisplayMessageActivity extends Activity {
     Sequencer sequencer;
-    PlaySound playSound;
     private String file = "exampleout.mid";
     private MediaPlayer mediaPlayer = new MediaPlayer();
     private String answer;
     private TextView answerText = null;
     private FileDescriptor fileDescriptor = new FileDescriptor();
 
+    // Middle C is integer 60
+    private static int[] majorPentatoic = {60,62,64,67,69,72};
+    private static int[] minorPentatoic = {60,63,65,67,70,72};
+    private static int[] major = {60,62,64,65,67,69,71,72};
+    private static int[] melodicMinor = {60,62,63,65,67,68,70,72};
+    private static int[] harmonicMinor = {60,62,63,65,67,69,71,72};
+    private static int[] chromatic = {60,61,62,63,64,65,66,67,68,69,70,71,72};
+
 	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-		// Get the message from the intent
+
+        //Get intents from user selections
 	    Intent intent = getIntent();
-	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         String scale = intent.getStringExtra(MainActivity.SCALE);
         String tempo = intent.getStringExtra(MainActivity.TEMPO);
         String melodyLength = intent.getStringExtra(MainActivity.MELODY_LENGTH);
@@ -56,48 +63,31 @@ public class DisplayMessageActivity extends Activity {
         //Play ogg files
         /*sequencer = new Sequencer(this, 4, 8);
         sequencer.setSample(0, R.raw.bass);
+        //Putting Kick on the beat
         sequencer.enableCell(0, 0);
         sequencer.enableCell(0,2);
         sequencer.enableCell(0,4);
         sequencer.enableCell(0,6);
         sequencer.play();*/
 
-        /*//Generate midi frequencies
-        playSound = new PlaySound(880,1);
-        playSound.genTone();
-        playSound.playSound();
-        playSound = new PlaySound(440,1);
-        playSound.genTone();
-        playSound.playSound();*/
-
-        // 1. Create some MidiTracks
+        // Create blank MidiTracks
         MidiTrack tempoTrack = new MidiTrack();
         MidiTrack noteTrack = new MidiTrack();
 
-        // 2. Add events to the tracks
-        // 2a. Track 0 is typically the tempo map
+        // Add events to the tracks
+        // Track 0 is typically the tempo map
         TimeSignature ts = new TimeSignature();
         ts.setTimeSignature(4, 4, TimeSignature.DEFAULT_METER, TimeSignature.DEFAULT_DIVISION);
 
+        //Set Tempo
         Tempo t = new Tempo();
         t.setBpm(Integer.parseInt(tempo));
-
         tempoTrack.insertEvent(ts);
         tempoTrack.insertEvent(t);
 
-        // 2b. Track 1 will have some notes in it
-        // Middle C is integer 60
-        int[] majorPentatoic = {60,62,64,67,69,72};
-        int[] minorPentatoic = {60,63,65,67,70,72};
-        int[] major = {60,62,64,65,67,69,71,72};
-        int[] melodicMinor = {60,62,63,65,67,68,70,72};
-        int[] harmonicMinor = {60,62,63,65,67,69,71,72};
-        int[] chromatic = {60,61,62,63,64,65,66,67,68,69,70,71,72};
-
+        //Create Melody
         int[] melody = {};
-        int mLength = Integer.parseInt(melodyLength);
-        int[] randMelody = new int[mLength];
-        Random generator = new Random();
+
 
         //Determine which button was pressed to play corresponding melody notes
         try {
@@ -121,6 +111,10 @@ public class DisplayMessageActivity extends Activity {
         }
 
         //Randomize melody notes
+        int mLength = Integer.parseInt(melodyLength);
+        int[] randMelody = new int[mLength];
+        Random generator = new Random();
+
         for (int i = 0; i < mLength; i++){
             if (i==0 ){
                 randMelody[i] = 60;
@@ -133,11 +127,12 @@ public class DisplayMessageActivity extends Activity {
 
         melody = randMelody.clone();
 
+        // Create Midi Track
         for(int i = 0; i < melody.length; i++)
         {
             int channel = 0, pitch = melody[i], velocity = 100;
             NoteOn on = new NoteOn(480 * i, channel, pitch, velocity);
-            NoteOff off = new NoteOff(i * 480 + 120, channel, pitch, 0);
+            NoteOff off = new NoteOff(i * 480 + 240, channel, pitch, 0);
 
             noteTrack.insertEvent(on);
             noteTrack.insertEvent(off);
@@ -150,7 +145,7 @@ public class DisplayMessageActivity extends Activity {
         // It's best not to manually insert EndOfTrack events; MidiTrack will
         // call closeTrack() on itself before writing itself to a file
 
-        // 3. Create a MidiFile with the tracks we created
+        // Create a MidiFile with the tracks we created
         ArrayList<MidiTrack> tracks = new ArrayList<MidiTrack>();
         tracks.add(tempoTrack);
         tracks.add(noteTrack);
